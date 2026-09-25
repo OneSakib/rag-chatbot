98593d6aa1465e48b564d2debf0f18c0c12882f8
Sakib Malik <maliksakib347@gmail.com>
2026-09-25 19:43:25 +0530

    docker compose updated

--- backend/app/worker/celery_app.py
+++ backend/app/worker/celery_app.py
@@ -2,8 +2,8 @@ from celery import Celery
 
 celery_app = Celery(
     "worker",
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0",  # for storing results
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0",  # for storing results
     include=["app.worker.tasks"],
 )
 
